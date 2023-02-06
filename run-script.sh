7f9ba67de4b49f2cfbda0cd254093884b82fa596
Kulbhushan <kulbhushan.mayer@gmail.com>
2023-02-06 13:32:28 +0530

    Added this change for GITHUB WEBHOOK trigger

--- run-script.sh
+++ run-script.sh
@@ -4,3 +4,4 @@ echo "[INFO] Current time is:"
 date
 echo "[INFO] Added this change for manual trigger"
 echo "[INFO] Added this change for POLL SCM Trigger"
+echo "[INFO] Added this change for GITHUB WEBHOOK trigger"
